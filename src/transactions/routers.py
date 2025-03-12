ac73e9ae109407cbc2adc73089fad85ad1c04ccc
Igor Chesnykh <igor.chesnyx@mail.ru>
2025-03-12 22:42:26 +0300

    Add payment confirmation

--- src/transactions/routers.py
+++ src/transactions/routers.py
@@ -7,6 +7,7 @@ from src.transactions.schemas import TransactionResponse, TransactionURLResponse
 from src.transactions.services import TransactionService
 
 from src.users.models import User, Plans
+from src.users.schemas import UserResponse
 from src.users.services import UserService
 
 router = APIRouter(tags=["transaction"], prefix="/transaction")
@@ -20,15 +21,26 @@ async def get_all_user_transactions(
     return list(map(lambda x: TransactionResponse(**x.to_dict()), transactions))
 
 
-# await UserService().update_user_plan(transaction.user_id, transaction.plan)
-
-
 @router.post("/new_payment", response_model=TransactionURLResponse)
 async def new_payment(
         current_user: Annotated[User, Depends(UserService().get_current_user)],
         plan: Plans
 ):
     return TransactionURLResponse(url=await TransactionService().create_transaction(current_user, plan))
+
+
+@router.post("/notifications", response_model=UserResponse)
+async def payment_confirm(request: Request) -> UserResponse:
+    req_json = await request.json()
+    print(req_json)
+
+    if req_json["event"] == "payment.succeeded":
+        payment_id = req_json["object"]["id"]
+
+        transaction = await TransactionService().confirm_payment(payment_id)
+        user = await UserService().update_user_plan(transaction.user_id, transaction.plan)
+
+        return UserResponse(**user.to_dict())
 
 
 @router.get("/is_confirmed/{idempotency_key}    ", response_model=CheckConfirmedResponse)
