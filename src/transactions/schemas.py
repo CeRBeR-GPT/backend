728652d1b10f946616863a8a1b4bd16f6e4c546a
Igor Chesnykh <igor.chesnyx@mail.ru>
2025-03-13 16:18:08 +0300

    Add get list plans endpoint

--- src/transactions/schemas.py
+++ src/transactions/schemas.py
@@ -1,9 +1,18 @@
 import uuid
 
 from datetime import datetime
+from typing import Literal
 from pydantic import BaseModel
 
 from src.users.models import Plans
+
+
+class PlanResponse(BaseModel):
+    name: str
+    max_length: int
+    count_limit: int
+    price: int
+    priority: Literal[1, 2, 3]
 
 
 class TransactionURLResponse(BaseModel):
