e7ecf48757f71e483ad5a9e981467b36643d1aff
Igor Chesnykh <igor.chesnyx@mail.ru>
2025-03-15 22:52:36 +0300

    Add celery tasks for email sending and daily reset available messages

--- utils/email_sender.py
+++ utils/email_sender.py
@@ -9,11 +9,11 @@ from config_data.config import load_config, EmailSender
 email_sender: EmailSender = load_config(".env").email_sender
 
 
-def _generate_code():
+def generate_confirmation_mode():
     return random.randint(email_sender.MIN_CODE, email_sender.MAX_CODE)
 
 
-def send_verification_code(email: str) -> int:
+def send_verification_code(email: str, code: int) -> int:
     smtp_server = smtplib.SMTP("smtp.gmail.com", 587)
     smtp_server.starttls()
     smtp_server.login(email_sender.EMAIL_NAME, email_sender.EMAIL_PASS)
@@ -23,7 +23,6 @@ def send_verification_code(email: str) -> int:
     msg['To'] = email
     msg['Subject'] = "Регистрация в AI-Chat"
 
-    code = _generate_code()
     body = f"Ваш код подтверждения: {code}\n\nЭто сообщение отправлено автоматически."
 
     msg.attach(MIMEText(body, 'plain'))
